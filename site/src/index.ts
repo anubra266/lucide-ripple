c4b2885ccd350dc36356ae769d9009a2d71f9fd4
anubra266 <anubra266@gmail.com>
2025-09-24 10:57:45 -0700

    fix prebuild

--- site/src/index.ts
+++ site/src/index.ts
@@ -1,7 +1,10 @@
 import { mount } from "ripple";
 import { App } from "./App.ripple";
-
 import "./styles.css";
+
+import { inject } from "@vercel/analytics"
+
+inject()
 
 mount(App, {
   target: document.getElementById("root"),
